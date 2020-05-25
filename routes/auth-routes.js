7d6cb1618804c8793e4ea6207a60d0094f23bc20
brittani1128 <brittani1128@gmail.com>
2020-05-24 22:41:13 -0500

    (feat):add passport setup and auth routes

---
+++ routes/auth-routes.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import passport from 'passport';
+
+const router = express.Router();
+
+//auth login
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+//auth logout
+router.get('/logout', (req, res) => {
+  //handle w passport
+  res.send('logging out');
+});
+
+//auth w/ google
+router.get('/google', passport.authenticate('google', {
+  scope: ['profile']
+}));
+
+// callback route 
+router.get('/google/redirect', passport.authenticate('google'), (req,res) => {
+  res.send('you reached the callback uri')
+});
+
+export default router;
